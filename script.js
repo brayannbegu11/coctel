618f5744375181a2ca61b459c3ee8623d4586535
brayannbegu11 <85093683+brayannbegu11@users.noreply.github.com>
2023-06-27 06:41:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const cursorElement = document.querySelector('.cursor');
 function processCommand(command) {
   if (command === 'sabores') {
     outputElement.innerHTML += 'Tropical<br>Picante<br>';
-  } else if (command === 'clear') {
+  } else if (command === 'borrar') {
     outputElement.innerHTML = '';
   } else if (command === 'ingredientes') {
     outputElement.innerHTML = '';
     outputElement.innerHTML += '<ul>';
-    outputElement.innerHTML += '<li>Tequila</li>';
+    outputElement.innerHTML += '<li>Ron</li>';
     outputElement.innerHTML += '<li>Licor de café</li>';
     outputElement.innerHTML += '<li>Zumo de limón</li>';
     outputElement.innerHTML += '<li>Syrup de frambuesa</li>';
@@ -23,7 +23,7 @@ function processCommand(command) {
     outputElement.innerHTML += '<ul>';
     outputElement.innerHTML += '<li>1 1/2 ron</li>';
     outputElement.innerHTML += '<li>1/2 Licor de café</li>';
-    outputElement.innerHTML += '<li>1/4Zumo de limón</li>';
+    outputElement.innerHTML += '<li>1/4 Zumo de limón</li>';
     outputElement.innerHTML += '<li>1/2 Syrup de frambuesa</li>';
     outputElement.innerHTML += '<li>1/4 shrup de piña con picante</li>';
     outputElement.innerHTML += '</ul>';
